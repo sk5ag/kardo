bed1bbbfee9fb494c3e55321d9db4173d50b48b5
Kardo <sk5ag.android@gmail.com>
2020-07-14 11:49:54 +0300

    after deleting unnecessary components - 14 Jul 2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material/material.module';
@@ -17,15 +16,11 @@ import { ModifyvisitComponent } from './visit/modifyvisit/modifyvisit.component'
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { EncounterComponent } from './encounter/encounter.component';
 import { PrescriptionTableComponent } from './prescription-table/prescription-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { TestformComponent } from './prescription-table/test-form/testform/testform.component';
-import { LabOrderTableComponent } from './lab-order-table/lab-order-table.component';
-import { DoctorNotesTableComponent } from './doctor-notes-table/doctor-notes-table.component';
-import { RadOrderTableComponent } from './rad-order-table/rad-order-table.component';
 import { TestsComponent } from './tests/tests.component';
 import { TestComponent } from './tests/test/test.component';
 import { TestService } from './shared/test.service';
@@ -37,17 +32,12 @@ import { AlltestsComponent } from './tests/alltests/alltests.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     ToolbarComponent,
     ListVisitsComponent,
     CreatevisitComponent,
     ModifyvisitComponent,
-    EncounterComponent,
     PrescriptionTableComponent,
     TestformComponent,
-    LabOrderTableComponent,
-    DoctorNotesTableComponent,
-    RadOrderTableComponent,
     TestsComponent,
     TestComponent,
     AlltestsComponent,
