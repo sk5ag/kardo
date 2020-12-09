0d8499728aa701a7a279d7f15f4e10bf1cef0e17
Kardo <sk5ag.android@gmail.com>
2020-12-09 19:18:23 +0300

    sorted some functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -51,61 +51,143 @@ exports.appointmentUpdate = functions.firestore.document('/{collection}/{id}')
     return null;
   })
 
-  
+
 //when status of appointment changes
 exports.visitCreate = functions.firestore.document('/{collection}/{id}')
-.onCreate((snap, context) => {
+  .onCreate((snap, context) => {
 
-  const collection = context.params.collection;
-  const values = snap.data();
-  const visitId = snap.ref.id;
+    const collection = context.params.collection;
+    const values = snap.data();
+    const visitId = snap.ref.id;
 
-  //const activities = admin.firestore().collection('activities');
-  const visits = admin.firestore().collection('visits');
+    //const activities = admin.firestore().collection('activities');
+    const visits = admin.firestore().collection('visits');
 
-  if (collection === 'visits') {
+    if (collection === 'visits') {
 
-    if (values.id != visitId) {
-      return visits.doc(visitId).update({
+      if (values.id != visitId) {
+        return visits.doc(visitId).update({
 
-        id: visitId,
+          id: visitId,
 
-      });
+        });
+      }
     }
-  }
-  return null;
-})
+    return null;
+  })
 
 
-//when a visit updated
-exports.visitUpdate = functions.firestore.document('/{collection}/{id}')
+//when an order has been added to a visit
+exports.updateOrders = functions.firestore.document('/{collection}/{id}')
   .onUpdate((snap, context) => {
 
     const collection = context.params.collection;
     const values = snap.after.data();
+    const docRef = snap.after.id;
+
+    console.log('The document reference is :: ', docRef);
 
     //const activities = admin.firestore().collection('activities');
     const orders = admin.firestore().collection('orders');
 
     if (collection === 'visits') {
 
-        console.log("ADD VisitOrder TO ORDERS COLLECTION");
-        return orders.add({
-
-          id: '',
-          patientName: values.patientName,
-          patientAge: values.patientAge,
-          patientGender: values.patientGender,
-          order: values.order,
-          prescription: values.prescription,
-          patientMobile: '',
-          patientBloodGroup: '',
-          patientLongtermIllness: '',
-          patientLongtermMedicine: '',
-          visitDescription: '',
-          isClosed: false,
-
-        });      
+      if (!values.order.length) {
+        console.log('ORDER Array is empty :: ', values.order);
+        if (orders.doc(docRef).path) {
+          console.log('Document Exist but has no orders. Remove the document from orders collection!');
+          return orders.doc(docRef).delete();
+        }
+      } else {
+        console.log('Order array containes data!');
+        if (orders.doc(docRef).path) {
+          console.log('Document Exist. Update the document inside orders collection!');
+          return orders.doc(docRef).set({
+            visitId: docRef,
+            patientName: values.patientName,
+            patientAge: values.patientAge,
+            patientGender: values.patientGender,
+            order: values.order,
+            patientMobile: values.patientMobile,
+            patientBloodGroup: values.patientBloodGroup,
+            patientLongtermIllness: values.patientLongtermIllness,
+            patientLongtermMedicine: values.patientLongtermMedicine,
+            isClosed: false
+          })
+        } else {
+          console.log('document does not exist, going to create it now');
+          return orders.doc(docRef).create({
+            visitId: docRef,
+            patientName: values.patientName,
+            patientAge: values.patientAge,
+            patientGender: values.patientGender,
+            order: values.order,
+            patientMobile: values.patientMobile,
+            patientBloodGroup: values.patientBloodGroup,
+            patientLongtermIllness: values.patientLongtermIllness,
+            patientLongtermMedicine: values.patientLongtermMedicine,
+            isClosed: false
+          })
+        }
+      };
     }
     return null;
   })
+
+  //when a prescription has been added to a visit
+exports.updatePrescriptions = functions.firestore.document('/{collection}/{id}')
+.onUpdate((snap, context) => {
+
+  const collection = context.params.collection;
+  const pvalues = snap.after.data();
+  const pdocRef = snap.after.id;
+
+  console.log('The document reference is :: ', pdocRef);
+
+  //const activities = admin.firestore().collection('activities');
+  const prescriptions = admin.firestore().collection('prescriptions');
+
+  if (collection === 'visits') {
+
+    if (!pvalues.prescription.length) {
+      console.log('PRESCRIPTIONS Array is empty :: ', pvalues.prescription);
+      if (prescriptions.doc(pdocRef).path) {
+        console.log('Document Exist but has no prescriptions. Remove the document from prescriptions collection!');
+        return prescriptions.doc(pdocRef).delete();
+      }
+    } else {
+
+      console.log('Prescriptions array containes data!', pvalues.prescription);
+      if (prescriptions.doc(pdocRef).path) {
+        console.log('Document Exist. Update the document inside prescriptions collection!');
+        return prescriptions.doc(pdocRef).set({
+          visitId: pdocRef,
+          patientName: pvalues.patientName,
+          patientAge: pvalues.patientAge,
+          patientGender: pvalues.patientGender,
+          order: pvalues.prescription,
+          patientMobile: pvalues.patientMobile,
+          patientBloodGroup: pvalues.patientBloodGroup,
+          patientLongtermIllness: pvalues.patientLongtermIllness,
+          patientLongtermMedicine: pvalues.patientLongtermMedicine,
+          isClosed: false
+        });
+      } else {
+        console.log('document does not exist, going to create it now');
+        return prescriptions.doc(pdocRef).create({
+          visitId: pdocRef,
+          patientName: pvalues.patientName,
+          patientAge: pvalues.patientAge,
+          patientGender: pvalues.patientGender,
+          order: pvalues.prescription,
+          patientMobile: pvalues.patientMobile,
+          patientBloodGroup: pvalues.patientBloodGroup,
+          patientLongtermIllness: pvalues.patientLongtermIllness,
+          patientLongtermMedicine: pvalues.patientLongtermMedicine,
+          isClosed: false
+        })
+      }
+    };
+  }
+  return null;
+})
