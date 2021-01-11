b50953ed34bc89d272d3bf31bc127e2c1d9e0c1e
Kardo <sk5ag.android@gmail.com>
2021-01-11 13:19:20 +0300

    medorder data uploaded successfully

--- init-medorder-db.ts
+++ init-medorder-db.ts
@@ -4,13 +4,14 @@ import {MEDORDERS} from './medorder_db';
 import * as firebase from 'firebase';
 
 var config = {  
-    apiKey: "AIzaSyDSatdRZPD2aUhyMOSALSuQ6kcHjojxTXU",
-    authDomain: "test-cad50.firebaseapp.com",
-    databaseURL: "https://test-cad50.firebaseio.com",
-    projectId: "test-cad50",
-    storageBucket: "test-cad50.appspot.com",
-    messagingSenderId: "3706584181",
-    appId: "1:3706584181:web:d59ec49b6cc03f19f94c3c"
+  apiKey: "AIzaSyDSatdRZPD2aUhyMOSALSuQ6kcHjojxTXU",
+  authDomain: "test-cad50.firebaseapp.com",
+  databaseURL: "https://test-cad50.firebaseio.com",
+  projectId: "test-cad50",
+  storageBucket: "test-cad50.appspot.com",
+  messagingSenderId: "3706584181",
+  appId: "1:3706584181:web:d59ec49b6cc03f19f94c3c"
+
 };
 
 console.log("Uploading data to the database with the following config:\n");
@@ -27,9 +28,8 @@ main().then(r => console.log('Done.'));
 async function uploadData() {
   const orders = await db.collection('medorders');
   for (let order of Object.values(MEDORDERS)) {
-      // await orders.add(order);
-    console.log(`Uploading med order ${order['medorder_title']["medorder_category"]}`);
-    
+    await orders.add(order);
+    console.log(`Uploading med order ${order["medorder_title"]["medorder_category"]}`);
   }
 }
 
